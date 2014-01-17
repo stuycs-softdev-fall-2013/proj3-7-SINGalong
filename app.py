12456cf5531828bf5fed5d7d3833ef2cb19000b2
Christine Xu <christinexu1001@gmail.com>
2014-01-17 12:54:53 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template, session, request, redirect, url_for
+import api
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        return render_template('home.html')
+
+@app.route('/AboutUs')
+def AboutUs():
+    return render_template('AboutUs.html')
+    
+        
+
+if __name__=="__main__":
+    app.debug=True
+    app.run(host='0.0.0.0',port=7004)
