e666beba973fe7f539227d750c5138234dbc1c40
Noah Rosenberg <nrosen56@gmail.com>
2014-01-24 22:51:18 -0500

    fixed up stuff with facebook oauth

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('test.html')
+
+
 if __name__ == "__main__":
     app.debug=True
     app.run('0.0.0.0')
-
-@app.route("/index")
-def index():
-    render_template('test.html')
