5112848b56ee6731dbd66e526a85a454e231373b
Maia Ezratty <maiaviolet@gmail.com>
2014-01-27 14:18:21 -0500

    starting mongo work

--- director.py
+++ director.py
@@ -1,5 +1,15 @@
 from pymongo import MongoClient
 
+connection = MongoClient('db.stuycs.org')
+db=connection.admin
+db.authenticate('softdev','softdev')
+
 client = MongoClient()
 directors = client.db.directors
 directors.remove()
+
+def addDirector(name):
+    db.directors.insert({'name': name})
+
+def removeDirector(name):
+    db.directors.update({'name': name})
