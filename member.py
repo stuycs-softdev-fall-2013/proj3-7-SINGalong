a0232604f475cbea658c66f775603e7cfd530b3d
Noah Rosenberg <nrosen56@gmail.com>
2014-01-27 17:14:41 -0500

    requiring FB auth to view pages

--- member.py
+++ member.py
@@ -6,10 +6,10 @@ db.authenticate('softdev','softdev')
 
 client = MongoClient()
 members = client.db.members
-members.remove()
+#members.remove() 
 
 def addMember(name):
-    db.members.insert({'name': name})
+    db.members.insert({'name': name, 'crew':crew})
 
 def removeMember(name):
     db.members.remove({'name': name})
